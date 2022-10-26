d3e3b439e17cd4208053cacdb85ff0018f98d6ab
Wesley Lima <114434400+Wusley@users.noreply.github.com>
2022-10-26 20:03:02 -0300

    Update redis.js

--- source/config/redis.js
+++ source/config/redis.js
@@ -1,10 +1,6 @@
 module.exports = {
   'host': 'pub-redis-10837.us-east-1-3.6.ec2.redislabs.com', // https://app.redislabs.com
-  // 'host': 'redis-thwess-4304475153.redis.irstack.com', // https://www.nodejitsu.com
   'port': 10837,
-  // 'port': 6379,
-  'pass': 'war@123'
-  // 'pass': 'redis-thwess-4304475153.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4'
+  'pass': ''
 };
 
-// redis-commander --redis-port 6379 --redis-host redis-thwess-4304475153.redis.irstack.com --redis-password redis-thwess-4304475153.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4
