fb2266f53b0ae8e9548ea0c7567d5a9b1eb13433
Wesley Lima <114434400+Wusley@users.noreply.github.com>
2022-10-26 20:03:35 -0300

    Update mongodb.js

--- source/config/mongodb.js
+++ source/config/mongodb.js
@@ -1,4 +1,3 @@
 module.exports = {
-  'connect': 'mongodb://warrr:warrr1234@ds051738.mongolab.com:51738/warrr' // https://mongolab.com
-  // 'connect': 'mongodb://nodejitsu:e11c0c5398776312171c8367f07edac4@troup.mongohq.com:10093/nodejitsudb7939248683' // https://www.nodejitsu.com
+  'connect': '' // https://mongolab.com
 };
